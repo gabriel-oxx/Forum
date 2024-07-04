cfceca7d182902c29947a5d2ea07aff9e7effa53
Gabriel <gabrielcsilvasg@gmail.com>
2024-07-04 20:56:21 -0300

    Adicionar migration versão 1

---
+++ src/main/resources/db/migration/V1__create-table-topics.sql
@@ -0,0 +1,10 @@
+CREATE TABLE topics
+(
+    id            BIGSERIAL PRIMARY KEY,
+    title         VARCHAR(100) NOT NULL,
+    description   VARCHAR(100) NOT NULL,
+    status        VARCHAR(10)  NOT NULL,
+    creation_date TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    author        VARCHAR(100) NOT NULL,
+    course        VARCHAR(200) NOT NULL
+);
