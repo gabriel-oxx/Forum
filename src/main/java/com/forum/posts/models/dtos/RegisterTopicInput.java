c5d21e7755c38f4deaab31b0069bfa7720644aea
Gabriel <gabrielcsilvasg@gmail.com>
2024-07-05 12:08:42 -0300

    Renomear DTO Topic.java para RegisterTopicInput.java

--- src/main/java/com/forum/posts/models/dtos/RegisterTopicInput.java
+++ src/main/java/com/forum/posts/models/dtos/RegisterTopicInput.java
@@ -4,7 +4,7 @@ import com.forum.posts.models.Status;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record RegisterTopic(
+public record RegisterTopicInput(
 		@NotBlank(message = "Título é obrigatório")
 		String title,
 		@NotBlank(message = "Descrição do problema é obrigatória")
